faa972bbf1966d6dbcd825e289acc69456020858
karuneshm95 <62456803+karuneshm95@users.noreply.github.com>
2023-07-04 16:37:53 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 selected_file = st.sidebar.selectbox('Select Dataset', ('CWRU_12K_3hp.parquet', 'CWRU_12K_2hp.parquet', 'CWRU_12K_1hp.parquet','CWRU_12K_0hp.parquet'))
 # Reset the warning filters after your Streamlit code
-warnings.resetwarnings()
+
 # Load the data
 @st.cache_data()
 def load_data(selected_file):
@@ -12,7 +12,7 @@ def load_data(selected_file):
     return data
 
 data = load_data(selected_file)
-warnings.resetwarnings()
+X_train, X_test, y_train, y_test = train_test_split(data.iloc[:,:-1], data.iloc[:,-1], test_size=0.2, random_state=42)
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -65,7 +65,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 # Split the data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(data.iloc[:,:-1], data.iloc[:,-1], test_size=0.2, random_state=42)
+
 
 # Sidebar - Select Classifier
 selected_classifier = st.sidebar.selectbox('Select Classifier', ('Logistic Regression', 'Random Forest', 'SVM','KNN','Decision Tree'))
